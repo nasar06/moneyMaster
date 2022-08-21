f2a977cde79df7c95e109a28884dad9ec8096bf1
nasar06 <108610784+nasar06@users.noreply.github.com>
2022-08-21 19:51:41 +0600

    Update money.js
    parcentage and total added

--- money.js
+++ money.js
@@ -1,14 +1,16 @@
 
 // get fild amount
-function getFilAmount(filId){
+function getFilAmount(filId) {
     const fildElement = document.getElementById(filId);
     const fildString = fildElement.value;
     const fildValue = parseFloat(fildString);
+
     return fildValue;
+
 }
 
 //set fil amount
-function setFildAmount(fildId, amount){
+function setFildAmount(fildId, amount) {
     const restFildElement = document.getElementById(fildId);
     const restFildString = restFildElement.innerText;
     const restFildAmount = parseFloat(restFildString);
@@ -19,7 +21,7 @@ function setFildAmount(fildId, amount){
 
 
 //click Calculate btn
-document.getElementById('calculateBtn').addEventListener('click', function(){
+document.getElementById('calculateBtn').addEventListener('click', function () {
     //get Income Value
     const getIncomeValue = getFilAmount('income');
 
@@ -29,19 +31,53 @@ document.getElementById('calculateBtn').addEventListener('click', function(){
     const getRentValue = getFilAmount('rent');
     //get clothes value
     const getClothesValue = getFilAmount('clothes');
-    
+
 
     //sum food + rent + clothes
     const totalCost = getFoodValue + getRentValue + getClothesValue;
-    
+
 
     //set total amount
     const setTotalAmount = setFildAmount('total-expenses', totalCost);
-    
+
 
     // minus income - total Cost
     const remainigTotal = getIncomeValue - totalCost;
 
     //set Remainig amount
-    const setRemainigAmount = setFildAmount('R-blance', remainigTotal )
+    const setRemainigAmount = setFildAmount('R-blance', remainigTotal)
+})
+
+function getOnlyAmount(fildId) {
+    const restFildElement = document.getElementById(fildId);
+    const restFildString = restFildElement.innerText;
+    const restFildAmount = parseFloat(restFildString);
+
+    return restFildAmount;
+}
+
+//click and get parcentage
+document.getElementById('save-btn').addEventListener('click', function () {
+    //get Parcentage value
+    const getParcentageValue = getFilAmount('saving-value');
+    // get Income value
+    const getIncomeValue = getFilAmount('income');
+
+    // calculate parcentage
+    const calParcentageAmount = getIncomeValue * getParcentageValue / 100;
+
+    //set saving amount
+    const setSavingAmount = setFildAmount('saving-amount', calParcentageAmount);
+
+    //get Blance amount
+    const getBlance = getOnlyAmount('R-blance');
+
+    //get Saving amount
+    const getSavingAmount = getOnlyAmount('saving-amount');
+
+    //sum Balance and Saving Amount
+    const blanceOrSaving = getBlance + getSavingAmount;
+
+    //set Remaining Blance total
+    const getMainTotal = setFildAmount('remaining-blance', blanceOrSaving);
 })
